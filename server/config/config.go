36fce16033f48f83355f37f7eadbdc4daeef1492
Vitaly Egorov <venomman88@gmail.com>
2023-01-29 17:10:55 +0300

    release 0.0.5

---
+++ server/config/config.go
@@ -0,0 +1,20 @@
+package config
+
+const (
+	// login DB
+	DbLogin = "login"
+
+	//pass DB
+	DbPass = "password"
+
+	// IP DB
+	DbIP = "localhost"
+
+	//port DB
+	DbPort = "port"
+
+	// Name BD
+	DB = "name"
+
+	ServerPort = 50051
+)
